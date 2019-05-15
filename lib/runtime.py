66b280faa74d6e7c0dae5702908733283861d890
Neil Ryan <soctar@gmail.com>
2019-05-15 16:33:52 -0700

    Start of multi-panel display, still buggy

--- lib/runtime.py
+++ lib/runtime.py
@@ -5,6 +5,12 @@ from prompt_toolkit.shortcuts import prompt
 from prompt_toolkit.styles import Style
 from prompt_toolkit.completion import Completer, Completion
 from prompt_toolkit.validation import Validator, ValidationError
+from prompt_toolkit.layout.containers import HSplit, Window
+from prompt_toolkit.widgets import TextArea
+from prompt_toolkit.layout.layout import Layout
+from prompt_toolkit.document import Document
+from prompt_toolkit.application import Application
+from prompt_toolkit.key_binding import KeyBindings
 
 COMMANDS = ['step', 'info', 'list', 'time']
 
@@ -53,6 +59,37 @@ class ModuleCompleter(Completer):
                 yield Completion(a, -len(word_before_cursor),
                                  display_meta=display_meta)
 
+class InputHandler():
+    def __init__(self, input_field, output_field):
+        self.input_field = input_field
+        self.output_field = output_field
+        self.sim_time = 0
+
+    def accept(self, buff):
+        out_text = ""
+        text = self.input_field.text.strip().split()
+        if text[0] == 'list':
+            for module in self.model.get_traced_modules():
+                out_text += f"{module.get_name()}\n"
+        elif text[0] == 'info':
+            modules = self.model.get_traced_modules()
+            req_module = [m for m in modules if m.get_name() == text[1]]
+            if not req_module:
+                out_text += "ERROR: Module not found!\n"
+            else:
+                out_text += f"{str(req_module[0])}\n"
+        elif text[0] == 'step':
+            if len(text) == 2:
+                num_steps = int(text[1])
+            else:
+                num_steps = 1
+            self.sim_time = self.model.update(self.sim_time, num_steps)
+        elif text[0] == 'time':
+            out_text += str(self.sim_time)
+
+        self.output_field.buffer.document = Document(text=out_text,
+                                                     cursor_position=len(out_text))
+
 class Runtime():
     def __init__(self, display, model):
         if display is not None:
@@ -61,46 +98,42 @@ class Runtime():
         self.prompt = None
 
     def create_prompt(self, module_names):
-        prompt_style = Style.from_dict({
-            'arrow': '#00aa00'
-        })
         prompt_message = [
             ('class:arrow', '> '),
         ]
-        completer = ModuleCompleter(module_names)
-        self.prompt = (prompt_message, prompt_style, completer)
+                                      # validate_while_typing=False,
+                                      # validator=InputValidator())
+        return TextArea(prompt='> ', style='class:arrow',
+                        completer=ModuleCompleter(module_names))
 
     def start(self):
         module_names = [m.get_name() for m in self.model.get_traced_modules()]
-        self.create_prompt(module_names)
-        session = PromptSession()
-        sim_time = 0
-        while 1:
-            try:
-                text = session.prompt(self.prompt[0], style=self.prompt[1],
-                                      completer=self.prompt[2],
-                                      validate_while_typing=False,
-                                      validator=InputValidator())
-            except EOFError:
-                exit(0)
-            except KeyboardInterrupt:
-                exit(0)
-            text = text.strip().split()
-            if text[0] == 'list':
-                for module in self.model.get_traced_modules():
-                    print(module.get_name())
-            elif text[0] == 'info':
-                modules = self.model.get_traced_modules()
-                req_module = [m for m in modules if m.get_name() == text[1]]
-                if not req_module:
-                    print("ERROR: Module not found!")
-                else:
-                    print(str(req_module[0]))
-            elif text[0] == 'step':
-                if len(text) == 2:
-                    num_steps = int(text[1])
-                else:
-                    num_steps = 1
-                sim_time = self.model.update(sim_time, num_steps)
-            elif text[0] == 'time':
-                print(sim_time)
+        input_field = self.create_prompt(module_names)
+        output_field = TextArea(text="asdf")
+        container = HSplit([
+            output_field,
+            Window(height=1, char='-'),
+            input_field
+        ])
+        style = Style([
+            ('arrow', '#00aa00')
+        ])
+
+        kb = KeyBindings()
+
+        @kb.add('c-c')
+        @kb.add('c-q')
+        def _(event):
+            " Pressing Ctrl-Q or Ctrl-C will exit the user interface. "
+            event.app.exit()
+
+        handler = InputHandler(input_field, output_field)
+
+        input_field.accept_handler = handler.accept
+        application = Application(
+            layout=Layout(container, focused_element=input_field),
+            mouse_support=True,
+            key_bindings=kb,
+            style=style,
+            full_screen=True)
+        application.run()
