ea7820ac935dce441661c3bb18ec1470a27ffa17
Neil Ryan <soctar@gmail.com>
2019-07-30 12:33:38 -0700

    Made manycore_model a better example to start with

--- manycore_model.py
+++ manycore_model.py
@@ -10,28 +10,57 @@ class ManycoreModel(DebugModel):
     """DebugModel that describes a 2x2 celerity manycore"""
     def gen_rf_module(self, core_x, core_y):
         """ Generate module for the given x,y core for the register file"""
+        # The RF module is a Memory DebugModule
         header = f"test_bsg_manycore.UUT.y[{core_y+1}].x[{core_x}].tile."
         header += "proc.h.z.hobbit0."
 
         addr = header + "rf_wa"
         wdata = header + "rf_wd"
         wen = header + "rf_wen"
+        # Memory modules take an address signal, wdata signal, wen signal,
+        # and the assertion level of the write enable signal (True for active
+        # high). Memory modules have a few additional flags:
+        #
+        # segments=['a', ('b', 'f'), '10']
+        #   Give a list of memory locations that we want to track or tuples
+        #   that describe a range of memory locations that we want to track.
+        #   The above example would track address 0xa, 0xb-0xf, and 0x10.
+        #
+        # size=32
+        #   If `size` is given, fill in memory locations that haven't been
+        #   written with don't cares in the display. If a size isn't given, we
+        #   lazily track memory locations as they are written to.
+        #
+        # show_signals=True
+        #   If true, show the address, wdata, and wen signals in the display
+        #   panel for this module. If false, only memory locations are shown.
+        #
         self.add_module(Memory(f"rf_{core_y}_{core_x}", addr, wdata, wen,
                                True, size=32,
                                show_signals=True))
 
     def gen_inst_module(self, core_x, core_y):
         """DebugModule for signals that describe the current instruction"""
+        # The Inst module is a Core Module -- useful for tracking program
+        # counters (and correlating with lines of source code).
         header = f"test_bsg_manycore.UUT.y[{core_y+1}].x[{core_x}].tile."
         header += "proc.h.z.hobbit0."
         inst_sigs = []
         pc_sig = header + "pc_real"
         inst_sigs.append(header + "exe.pc_plus4")
         inst_sigs.append(header + "id.pc_plus4")
+        # The second argument to a Core module (after the name) is the name of
+        # the program counter signal. The program counter needs to correlate
+        # with addresses in the binary (if one is provided) -- in this case,
+        # we needed to make a new signal, pc_real, that adds the extra two bits
+        # of zeros that would typically be ommitted.
+        # The third argument is a list of signals that should also be tracked
+        # by this module.
         self.add_module(Core(f"inst_{core_y}_{core_x}", pc_sig, inst_sigs))
 
     def gen_wmem_module(self, core_x, core_y):
         """DebugModule for signals that write to the local memory"""
+        # The wmem module is a BasicModule
         header = f"test_bsg_manycore.UUT.y[{core_y+1}].x[{core_x}].tile."
         header += "proc.h.z.hobbit0."
         addr = header + 'to_mem_o.addr'
@@ -40,6 +69,7 @@ class ManycoreModel(DebugModel):
         isstr = header + 'mem.decode.is_store_op'
         stall = header + 'stall'
         mem_sigs = [addr, wdata, isload, isstr, stall]
+        # Basic Modules only take a list of signals to be tracked
         self.add_module(BasicModule(f"wmem_{core_y}_{core_x}", mem_sigs))
 
     def gen_remote_module(self, core_x, core_y):
@@ -71,6 +101,9 @@ class ManycoreView(Display):
         insts = []
         wmem = []
         remote = []
+
+        # This represents a fairly standard paradigm for creating Displays.
+        # We start by creating a "View" of each module that we want to display.
         for i in range(2):
             for j in range(2):
                 regs.append(View(model.get_module(f"rf_{i}_{j}")))
@@ -78,6 +111,7 @@ class ManycoreView(Display):
                 wmem.append(View(model.get_module(f"wmem_{i}_{j}")))
                 remote.append(View(model.get_module(f"remote_{i}_{j}")))
 
+        # Then, we arrange the views with HSplits and VSplits.
         regs = HSplit(
             VSplit(
                 HSplit(VSplit(regs[0], remote[0]),
